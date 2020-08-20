8cd7e127dd14e1314b668aaaa8ccba786b357795
Walter Rodrigues 'Rx <walterrx@gmail.com>
2020-08-20 16:35:43 -0300

    Add files via upload

---
+++ apl-angular-client/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+
+    restItems: any;
+  restItemsUrl = 'http://localhost:8075/api/tweets/svc/v1/tweets?view=obterRetorno';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getRestItems();
+  }
+
+  // Read all REST Items
+  getRestItems(): void {
+    this.restItemsServiceGetRestItems()
+      .subscribe(
+        restItems => {
+          this.restItems = restItems;
+          console.log(this.restItems);
+        }
+      )
+  }
+
+  // Rest Items Service: Read all REST Items
+  restItemsServiceGetRestItems() {
+    return this.http
+      .get<any[]>(this.restItemsUrl)
+      .pipe(map(data => data));
+  }
+}
+
+
